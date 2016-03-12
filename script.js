328b1bedd09c5dd64caa9cf70de99f960f52c4de
Alex <mail2zin@gmail.com>
2016-03-12 15:01:07 +0100

    onclick handler

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ var db = new Firebase('https://blinding-inferno-4181.firebaseio.com/');
 var root = this;
 var p1, p2;
 
-$('#nameInput').keypress(function(e) {
-    if (e.keyCode == 13) {
+$(document).ready(function() {
+    $('#nameInput').val('Player1');
+
+    $('#start').on('click', function(e) {
         var name = $('#nameInput').val();
         var room = db.child("room");
         if (room && !room.p1) { //we are 1st
@@ -17,26 +19,12 @@ $('#nameInput').keypress(function(e) {
             root.p1 = room.p1;
             root.p2 = name;
         }
-    }
-});
+    });
 
-db.on('child_added', function(snapshot) {
-    var message = snapshot.val();
-    displayChatMessage(message.name, message.text);
-    console.log(message.name, message.text);
-});
+    db.on("value", function(snapshot) {
+        $('#dbcontent').html(JSON.stringify(snapshot.val(), null, 2));
+    }, function(errorObject) {
+        $('#dbcontent').html("The read failed: " + errorObject.code);
+    });
 
-function displayChatMessage(name, text) {
-    $('<div/>').text(text).prepend($('<em/>').text(name + ': ')).appendTo($('#messagesDiv'));
-    $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
-};
-
-function onstart() {
-    console.log(root.p1, root.p2);
-}
-
-db.on("value", function(snapshot) {
-    $('#dbcontent').html(JSON.stringify(snapshot.val(), null, 2));
-}, function(errorObject) {
-    $('#dbcontent').html("The read failed: " + errorObject.code);
 });
