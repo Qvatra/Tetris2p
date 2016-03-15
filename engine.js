47fd42e642d38add7a6eef0902e70993007c532e
Alex <mail2zin@gmail.com>
2016-03-15 23:30:00 +0100

    ++

--- engine.js
+++ engine.js
@@ -6,38 +6,32 @@ var Engine = (function(dimention) {
     var block = [];
     var dir;
 
-    // private methods
-
     function checkState(field) {
-        if (block.length !== 0 && !canBeMoved(field, down)) {
+        if (block.length !== 0 && !canBeMoved(field, down)) { // if block can't move down anymre
             mergeBlock(field);
+            checkLines(field);
         }
     }
 
-    // merges an active block with the field if block can't move down anymre    
+    // merges an active block with the field 
     function mergeBlock(field) {
         block.forEach(function(item) {
             field[item.y][item.x] = dir;
         });
         block = [];
-        // check if line should be blowed up
-        checkLines(field);
     }
 
-    // checking that all elements of each line marked in positive or negative numbers
+    // check for complete lines and blow'em up
     function checkLines(field) {
         for (var y = 0; y < dimention[1]; y++) {
-
             var lineComplete = field[y].every(function(item, x) {
                 return field[y][x] === field[y][(x + 1) % dimention[0]];
             });
-
             // ..and if so, blow the line up
             if (lineComplete) {
                 for (var x = 0; x < dimention[0]; x++) {
                     field[y][x] = Render.isNeutral(x, y) ? 0 : -field[y][x];
                 }
-
                 // ..and make all top blocks fall
                 if (dir === 1) { // ..down
                     for (var row = y - 1; row >= 0; row--) {
@@ -60,7 +54,7 @@ var Engine = (function(dimention) {
         }
     }
 
-    // init new falling black
+    // init new falling block
     function initBlock(field) {
         block = [{ x: dimention[0] / 2, y: dir > 0 ? 0 : dimention[1] - 1 }];
         block.forEach(function(item) {
@@ -82,16 +76,16 @@ var Engine = (function(dimention) {
     function moveCell(field, cell, direction) {
         var markup = field[cell.y][cell.x]; // markup is a number on a field
         field[cell.y][cell.x] = Render.isNeutral(cell.x, cell.y) ? 0 : -markup / Math.abs(markup); // mark prev position as opposite to our field color
-        var newX = direction(cell).x; // eval new position regarding moving direction
-        var newY = direction(cell).y;
-        field[newY][newX] = markup; // assign our markup to new position on the field
-        return { x: newX, y: newY }; // return cells new coordinates
+        var p = direction(cell);   // eval new position regarding moving direction
+        field[p.y][p.x] = markup;  // assign our markup to new position on the field
+        return { x: p.x, y: p.y }; // return cells new coordinates
     }
 
     function moveBlock(field, direction) {
-        if (!field || !canBeMoved(field, direction)) return;
-        //console.log('move from ' + block[0].x + ':' + block[0].y + ' to ' + direction(block[0]).x + ':' + direction(block[0]).y);
-
+        if (!field || !canBeMoved(field, direction)) {
+            return;
+        }
+        
         block = block.map(function(blockCell) {
             return moveCell(field, blockCell, direction);
         });
@@ -114,27 +108,24 @@ var Engine = (function(dimention) {
         if (keypress === 0) {
             return;
         }
-        if (keypress === 97) {//a
+        if (keypress === 97) {  // a
             moveBlock(field, left);
         }
-        if (keypress === 100) {//d
+        if (keypress === 100) { // d
             moveBlock(field, right);
         }
-        if (keypress === 115) {//s
+        if (keypress === 115) { // s
             moveBlock(field, down);
         }
         keypress = 0;
     }
 
-    // public methods
     module.tick = function(field, keypress) {
         if (block.length === 0) {
             initBlock(field);
-            //console.info('init..');
         } else {
             moveBlock(field, down);
         }
-        //console.log("before pa");
         playerAction(field, keypress);
         checkState(field);
         return field;
